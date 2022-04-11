9c7e26c122245e0cdd45b794988b05f372d9d04a
alekahrens <scaaja@gmail.com>
2022-04-11 19:44:38 -0400

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.students, name = 'students')
+    path("", views.students, name = 'students'),
+    path("student/results", views.results)
 ]
