ac91e82292d0a48bb32026888d4ceefdec5ed9a8
Shamshad Abdullah <abdullas@clarkson.edu>
2022-04-13 08:30:36 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User   
 
 def main():
     """Run administrative tasks."""
