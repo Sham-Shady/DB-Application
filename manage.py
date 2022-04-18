7c51dd6535eabee2677a75ede5aa9f6926089e01
StudioOddity <101125600+StudioOddity@users.noreply.github.com>
2022-04-18 18:52:43 -0400

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.contrib.auth.models import User   
+
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courseProject.settings')
+
 
     try:
         from django.core.management import execute_from_command_line
