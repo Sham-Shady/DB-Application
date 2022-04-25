b46b5ae40462d3db670dda4b0905a0acb0f4e2ae
StudioOddity <101125600+StudioOddity@users.noreply.github.com>
2022-04-25 11:35:50 -0400

    Update views.py

--- professor/views.py
+++ professor/views.py
@@ -65,7 +65,7 @@ def professor(request):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd='KazumaK1ryu!',  # "mypassword",
+        passwd='password',  # "mypassword",
         auth_plugin='mysql_native_password',
         database="university",
     )
